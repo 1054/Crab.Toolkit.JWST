ab636b075577b45966693ac73d0dfb49af1b7319
1054 <>
2022-07-15 17:11:12 +0200

    Updates

--- SETUP.bash
+++ SETUP.bash
@@ -5,7 +5,7 @@
 Crab_BIN_SETUP_SCRIPT=$(dirname "${BASH_SOURCE[0]}")/bin/bin_setup.bash
 
 if [[ $- == *i* ]]; then
-    source "$Crab_BIN_SETUP_SCRIPT" -check casa-ms-split casa-ms-concat pdbi-uvt-go-splitpolar pdbi-uvt-go-import-uvfits pdbi-uvt-go-average pdbi-uvt-go-uvfit
+    source "$Crab_BIN_SETUP_SCRIPT" -check go-jwst-download-by-proposal-id.py
 else
     source "$Crab_BIN_SETUP_SCRIPT"
 fi
