2c6b2573c377cdf99b12b2bc9c2c7cb24139275e
1054 <>
2022-07-20 15:27:00 +0200

    Updates

--- bin/go-jwst-nircam-stage-3-task-1.py
+++ bin/go-jwst-nircam-stage-3-task-1.py
@@ -134,7 +134,7 @@ if __name__ == '__main__':
     # Print JWST pipeline version
     logger.info('JWST pipeline version: {}'.format(jwst.__version__))
     
-    # check CRDS 
+    # Check CRDS 
     try:
         logger.info("CRDS_PATH: {}".format(os.environ['CRDS_PATH']))
     except KeyError:
@@ -146,7 +146,13 @@ if __name__ == '__main__':
     except KeyError:
         logger.error("Error! CRDS_SERVER_URL environment variable not set!")
         sys.exit(-1)
-
+    
+    # Read command line input arguments
+    overwrite = False
+    for i in range(1, len(sys.argv)):
+        if sys.argv[i] == '--overwrite' or sys.argv[i] == '-overwrite':
+            overwrite = True
+    
     
     # Find all "jw*/calibrated2_cals/jw*_cal.fits"
     input_files = glob.glob("jw*/calibrated2_cals/jw*_cal.fits")
@@ -208,13 +214,27 @@ if __name__ == '__main__':
     # prepare association file to process all rate files into one single output file
     for subgroup_key, subgroup_table in zip(unique_groups.groups.keys, unique_groups.groups):
         
-        # 
-        # TODO: Need to find groups
-        #     "jw<sci>_<group>_<scan>_<instr>_(uncal|rate|cal).fits"
-        # 
-        # The Stage 2 pipeline can be called on a single fits file, or a collection of fits files. 
-        # When calling on multiple files, the input is a json-formatted file called an "association" 
-        # file that lists each of the fits files to be processed.
+        input_files = subgroup_table['file_path'].data.tolist()
+        output_name = 'jw{}_obs{}_{}_{}'.format(
+            subgroup_key['program'], 
+            subgroup_key['obs_id'], 
+            subgroup_key['instrument'],
+            subgroup_key['filter'])
+        output_dir = os.path.join('calibrated3_mosaics', output_name) # directly output to "calibrated3_mosaics" under current directory.
+        output_file = output_name+'.fits'
+        output_filepath = os.path.join(output_dir, output_file)
+        if not os.path.isdir(output_dir):
+            os.makedirs(output_dir)
+        else:
+            # check existing output file, skip the processing or backup and overwrite it. 
+            if os.path.isfile(output_filepath):
+                if not overwrite:
+                    logger.info("Found processed {} -> {} and overwrite is set to False. We will skip the processing.".format(input_files, output_filepath))
+                    continue
+                else:
+                    shutil.move(output_filepath, output_filepath+'.backup')
+        
+        # prepare the json-format "association" file
         asn_dict = OrderedDict()
         asn_dict['asn_type'] = 'None'
         asn_dict['asn_rule'] = 'DMS_Level3_Base'
@@ -226,15 +246,6 @@ if __name__ == '__main__':
         asn_dict['asn_id'] = subgroup_key['obs_id'] # TODO
         asn_dict['asn_pool'] = 'none'
         asn_dict['products'] = []
-        input_files = subgroup_table['file_path'].data.tolist()
-        output_name = 'jw{}_obs{}_{}_{}'.format(
-            subgroup_key['program'], 
-            subgroup_key['obs_id'], 
-            subgroup_key['instrument'],
-            subgroup_key['filter'])
-        output_dir = os.path.join('calibrated3_mosaics', output_name) # directly output to "calibrated3_mosaics" under current directory.
-        if not os.path.isdir(output_dir):
-            os.makedirs(output_dir)
         product_dict = OrderedDict()
         product_dict['name'] = output_name
         product_dict['members'] = []
@@ -257,8 +268,6 @@ if __name__ == '__main__':
         
         
         # prepare a single output file 
-        output_file = output_name+'.fits'
-        output_filepath = os.path.join(output_dir, output_file)
         
         logger.info("Processing {} -> {}".format(input_files, output_filepath))
         
