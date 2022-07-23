6568b3a075e2f69a16273b6a582db2eed99e220a
1054 <1054@users.noreply.github.com>
2022-07-23 12:30:19 +0200

    Updating tweakreg_detect_thresh_for_miri_image

---
+++ patches/20220723_tweakreg_detect_thresh_for_miri_image/dzliu_tweakreg_catalog_patch.apply.sh
@@ -0,0 +1,1 @@
+patch Software/CONDA/miniconda3/lib/python3.9/site-packages/jwst/tweakreg/tweakreg_catalog.py < dzliu_tweakreg_catalog_patch.txt
---
+++ patches/20220723_tweakreg_detect_thresh_for_miri_image/dzliu_tweakreg_catalog_patch.create.sh
@@ -0,0 +1,2 @@
+diff -u Software/CONDA/miniconda3/lib/python3.9/site-packages/jwst/tweakreg/tweakreg_catalog.py.backup Software/CONDA/miniconda3/lib/python3.9/site-packages/jwst/tweakreg/tweakreg_catalog.py.new > dzliu_tweakreg_catalog_patch.txt
+
