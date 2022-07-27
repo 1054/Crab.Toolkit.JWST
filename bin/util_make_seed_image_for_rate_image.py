e7b004f04ffa25dbbf70f999f2dbe8ef90582911
1054 <>
2022-07-28 01:19:53 +0200

    Updates

--- bin/util_make_seed_image_for_rate_image.py
+++ bin/util_make_seed_image_for_rate_image.py
@@ -32,6 +32,16 @@ from astropy.nddata.bitmask import (
 )
 
 from jwst.datamodels.dqflags import pixel as dqflags_pixel
+#{'GOOD': 0, 
+#'DO_NOT_USE': 1, 'SATURATED': 2, 'JUMP_DET': 4, 'DROPOUT': 8, 
+#'OUTLIER': 16, 'PERSISTENCE': 32, 'AD_FLOOR': 64, 'RESERVED_4': 128, 
+#'UNRELIABLE_ERROR': 256, 'NON_SCIENCE': 512, 'DEAD': 1024, 'HOT': 2048, 
+#'WARM': 4096, 'LOW_QE': 8192, 'RC': 16384, 'TELEGRAPH': 32768, 
+#'NONLINEAR': 65536, 'BAD_REF_PIXEL': 131072, 'NO_FLAT_FIELD': 262144, 'NO_GAIN_VALUE': 524288, 
+#'NO_LIN_CORR': 1048576, 'NO_SAT_CHECK': 2097152, 'UNRELIABLE_BIAS': 4194304, 'UNRELIABLE_DARK': 8388608, 
+#'UNRELIABLE_SLOPE': 16777216, 'UNRELIABLE_FLAT': 33554432, 'OPEN': 67108864, 'ADJ_OPEN': 134217728, 
+#'UNRELIABLE_RESET': 268435456, 'MSA_FAILED_OPEN': 536870912, 'OTHER_BAD_PIXEL': 1073741824, 'REFERENCE_PIXEL': 2147483648
+#}
 
 from jwst.datamodels import ImageModel
 
@@ -99,7 +109,7 @@ def make_seed_image_for_rate_image(
                 )
         dqmask = bitfield_to_boolean_mask(
                     image_model.dq,
-                    interpret_bit_flags('~(DO_NOT_USE+NON_SCIENCE+MSA_FAILED_OPEN+OTHER_BAD_PIXEL)', # SkyMatchStep().dqbits is '~DO_NOT_USE+NON_SCIENCE' in default
+                    interpret_bit_flags('~(DO_NOT_USE,NON_SCIENCE,SATURATED,RESERVED_4)', # SkyMatchStep().dqbits is '~DO_NOT_USE+NON_SCIENCE' in default
                         flag_name_map=dqflags_pixel), 
                     good_mask_value=1,
                     dtype=np.uint8
