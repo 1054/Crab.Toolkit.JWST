fd35b6b35913113dcc15eea5cc929c9222c16373
1054 <>
2022-09-11 18:45:38 +0200

    Updates

---
+++ bin/go-jwst-imaging-stage-3-step-2-make-dark-rates.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+# 
+# Make source emission seed image from the input mosaic image, then mask rate image with that seed, 
+# so that the masked rate image is like a darkobs.
+# 
+
+# Set necessary system variables
+if [[ -z "$CRDS_PATH" ]]; then
+    export CRDS_PATH=$HOME/jwst_crds_cache
+fi
+if [[ -z "$CRDS_SERVER_URL" ]]; then
+    export CRDS_SERVER_URL=https://jwst-crds.stsci.edu
+fi
+
+# Get script_dir
+script_dir=$(dirname "${BASH_SOURCE[0]}")
+
+# Read user input
+if [[ $# -lt 2 ]]; then
+    echo "Please input:"
+    echo "    mosaic_image_i2d.fits"
+    echo "    mosaic_image_i2d_asn.json"
+    echo "Notes:"
+    echo "    The second input JWST association file should contain many cal images, "
+    echo "    for which we will parse the names to get rate images and mask out "
+    echo "    source emission with the mosaic seed, so that they are like darkobs."
+    exit 255
+fi
+iarg=1
+overwrite=0
+mosaic_image=""
+mosaic_asn=""
+while [[ $iarg -le $# ]]; do
+    argstr="${!iarg}"
+    if [[ "$argstr" == "--overwrite" ]]; then
+        overwrite=1
+    elif [[ "$mosaic_image"x == ""x ]]; then
+        mosaic_image="${!iarg}"
+        echo "mosaic_image = \"mosaic_image\""
+    elif [[ "$mosaic_asn"x == ""x ]]; then
+        mosaic_asn="${!iarg}"
+        echo "mosaic_asn = \"mosaic_asn\""
+    fi
+    iarg=$((iarg+1))
+done
+
+
+
+this_file_in="$mosaic_image"
+this_file_out=$(echo "$this_file_in" | perl -p -e 's/\.fits$//g')"_remstriping_galaxy_seed_image.fits"
+this_dir_out=$(dirname "$this_file_out")
+if [[ ! -f "$this_file_out" ]] || [[ $overwrite -gt 0 ]]; then
+    echo $script_dir/remstriping "$this_file_in" \
+        --no-apply-flat \
+        --no-inplace \
+        --output-dir "$this_dir_out" \
+        --output-suffix "remstriping" \
+        --smooth 3.0
+    $script_dir/remstriping "$this_file_in" \
+        --no-apply-flat \
+        --no-inplace \
+        --output-dir "$this_dir_out" \
+        --output-suffix "remstriping" \
+        --smooth 3.0
+fi
+if [[ ! -f "$this_file_out" ]]; then
+    echo "Error! Failed to produce the output files: \"$this_file_out\""
+    exit 255
+fi
+
+seed_image="$this_file_out"
+
+# find all associated cal.fits and rate.fits, mask them with source emission mask
+cal_images=($(cat "$mosaic_asn" | grep "expname" | perl -p -e 's%[^0-9a-zA-Z_/.+-]% %g' | awk '{print $2}'))
+rate_images=()
+masked_rate_images=()
+for (( k = 0; k < ${#cal_images[@]}; k++ )); do
+    rate_image=$(echo "${cal_images[k]}" | perl -p -e 's%/calibrated2_cals/%/calibrated1_rates/%g' | perl -p -e 's%_cal.fits$%_rate.fits%g')
+    masked_rate=$(echo "$rate_image" | perl -p -e 's%\.fits$%%g')"_masked_source_emission.fits"
+    # make source-emission-masked rate image
+    if [[ ! -f "$masked_rate" ]] || [[ $overwrite -gt 0 ]]; then
+        echo $script_dir/util_mask_rate_data_with_seed_image.py \
+            "$rate_image" \
+            "$seed_image" \
+            "$masked_rate"
+        $script_dir/util_mask_rate_data_with_seed_image.py \
+            "$rate_image" \
+            "$seed_image" \
+            "$masked_rate"
+    fi
+    if [[ ! -f "$masked_rate" ]]; then
+        echo "Error! Failed to produce the output files: \"$masked_rate\""
+        exit 255
+    fi
+    rate_images+=("$rate_image")
+    masked_rate_images+=("$masked_rate")
+done
+
+
+
+
+
+
---
+++ bin/go-jwst-imaging-stage-3-step-3-merge-dark-rates.sh
@@ -0,0 +1,125 @@
+#!/bin/bash
+# 
+# Merge a list of masked rate images.
+# 
+
+# Set necessary system variables
+if [[ -z "$CRDS_PATH" ]]; then
+    export CRDS_PATH=$HOME/jwst_crds_cache
+fi
+if [[ -z "$CRDS_SERVER_URL" ]]; then
+    export CRDS_SERVER_URL=https://jwst-crds.stsci.edu
+fi
+
+# Get script_dir
+script_dir=$(dirname "${BASH_SOURCE[0]}")
+
+# Read user input
+if [[ $# -lt 2 ]]; then
+    echo "Please input:"
+    echo "    mosaic_image_1_i2d_asn.json"
+    echo "    mosaic_image_2_i2d_asn.json"
+    echo "Notes:"
+    echo "    The inputs are all JWST association files, which should contain many cal images, "
+    echo "    for which we will parse the names to get the so-called masked rate images, "
+    echo "    then merge them into one data, but excluding each self data."
+    exit 255
+fi
+iarg=1
+overwrite=0
+mosaic_asn_files=()
+while [[ $iarg -le $# ]]; do
+    argstr="${!iarg}"
+    if [[ "$argstr" == "--overwrite" ]]; then
+        overwrite=1
+    else
+        mosaic_asn_files+=("${!iarg}")
+        echo "mosaic_asn_files += \"${!iarg}\""
+    fi
+    iarg=$((iarg+1))
+done
+
+# get all cal images in the asn files
+
+multiobs_cal_images=()
+multiobs_rate_images=()
+multiobs_masked_rate_images=()
+for (( i = 0; i < ${#mosaic_asn_files[@]}; i++ )); do
+    temp_mosaic_asn="${mosaic_asn_files[k]}"
+    temp_cal_images=($(cat "$temp_mosaic_asn" | grep "expname" | perl -p -e 's%[^0-9a-zA-Z_/.+-]% %g' | awk '{print $2}'))
+    for (( m = 0; m < ${#temp_cal_images[@]}; m++ )); do
+        cal_image="${temp_cal_images[m]}"
+        rate_image=$(echo "$cal_image" | perl -p -e 's%/calibrated2_cals/%/calibrated1_rates/%g' | perl -p -e 's%_cal.fits$%_rate.fits%g')
+        masked_rate=$(echo "$rate_image" | perl -p -e 's%\.fits$%%g')"_masked_source_emission.fits"
+        multiobs_cal_images+=("$cal_image")
+        multiobs_rate_images+=("$rate_image")
+        multiobs_masked_rate_images+=("$masked_rate")
+    done
+done
+
+
+# loop each cal/rate image dir, merge all other source-emission-masked rates
+for (( i = 0; i < ${#multiobs_rate_images[@]}; i++ )); do
+    cal_image="${multiobs_cal_images[i]}"
+    rate_image="${multiobs_rate_images[i]}"
+    masked_rate_image="${multiobs_masked_rate_images[i]}"
+    merged_masked_rate=$(dirname "$masked_rate_image")"/merged_other_visits_rate_masked_source_emission.fits"
+    output_cal_image=$(echo "$cal_image" | perl -p -e 's/_cal.fits$/_cal_bkgsub_with_source_emission_mask.fits/g')
+    
+    if [[ ! -f "$merged_masked_rate" ]] || [[ $overwrite -gt 0 ]]; then
+        applicable_masked_rate_images=()
+        for (( m = 0; m < ${#multiobs_masked_rate_images[@]}; m++ )); do
+            if [[ "${multiobs_masked_rate_images[m]}" != "$masked_rate_image" ]]; then
+                applicable_masked_rate_images+=("${multiobs_masked_rate_images[m]}")
+            fi
+        done
+        
+        echo $script_dir/util_merge_source_emission_masked_rate_data.py \
+            ${applicable_masked_rate_images[@]} \
+            "$merged_masked_rate"
+        $script_dir/util_merge_source_emission_masked_rate_data.py \
+            ${applicable_masked_rate_images[@]} \
+            "$merged_masked_rate"
+        echo "# $script_dir/util_merge_source_emission_masked_rate_data.py" > "$merged_masked_rate_list_file"
+        for (( m = 0; m < ${#applicable_masked_rate_images[@]}; m++ )); do
+            echo "${applicable_masked_rate_images[m]}" >> "$merged_masked_rate_list_file"
+        done
+        
+        if [[ -f "$output_cal_image" ]]; then
+            mv "$output_cal_image" "$output_cal_image.backup"
+        fi
+        
+    fi
+    if [[ ! -f "$merged_masked_rate" ]]; then
+        echo "Error! Failed to produce the output files: \"$merged_masked_rate\""
+        exit 255
+    fi
+    
+    
+    # then also re-run stage2
+    if [[ ! -f "$output_cal_image" ]] || [[ $overwrite -gt 0 ]]; then
+        proc_args=(--darkobs "$merged_masked_rate")
+        if [[ $overwrite -gt 0 ]]; then
+            proc_args+=(--overwrite)
+        fi
+        echo "*** Running ***" $script_dir/go-jwst-imaging-stage-2-step-3-redo-bkgsub.py \
+            "$rate_image" \
+            "$output_cal_image" \
+            "${proc_args[@]}"
+        $script_dir/go-jwst-imaging-stage-2-step-3-redo-bkgsub.py \
+            "$rate_image" \
+            "$output_cal_image" \
+            "${proc_args[@]}"
+        if [[ ! -f "$output_cal_image" ]] && [[ ! -L "$output_cal_image" ]]; then
+            echo "Error! Failed to produce the output file: $output_cal_image"
+            exit 255
+        fi
+    fi
+    
+done
+
+
+
+
+
+
