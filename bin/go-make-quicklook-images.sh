a8ccef9c68e29b5c83d9dec8f3ce24e1c9dc5063
1054 <>
2022-11-02 17:18:22 +0100

    Updates

--- bin/go-make-quicklook-images.sh
+++ bin/go-make-quicklook-images.sh
@@ -4,6 +4,14 @@ set -e
 
 script_dir=$(dirname "${BASH_SOURCE[0]}")
 
+if [[ $# -eq 0 ]]; then
+    echo "Usage: "
+    echo "    Please input fits files."
+    exit 255
+fi
+
+echo "script_dir: $script_dir"
+
 for (( i = 1; i <= $#; i++ )); do
     filepath="${!i}"
     if [[ "$filepath" == *".fits" ]]; then
