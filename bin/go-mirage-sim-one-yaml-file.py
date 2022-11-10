7ed3cba520914a6862fc706fc4bc053046a694de
1054 <>
2022-11-10 21:19:39 +0100

    Updates

--- bin/go-mirage-sim-one-yaml-file.py
+++ bin/go-mirage-sim-one-yaml-file.py
@@ -219,9 +219,13 @@ def main(
     ABMAG = ((photmjsr * u.MJy/u.sr) * (pixar_sr * u.sr)).to(u.ABmag)
     logger.info('ABMAG: {}'.format(ABMAG))
     PHOTFNU = ABMAG.to(u.erg/u.s/u.cm**2/u.Hz)
+    logger.info('PHOTFNU: {}'.format(PHOTFNU))
     Pivot_wave = pivot_wave * u.um
+    logger.info('Pivot_wave: {}'.format(Pivot_wave))
     PHOTFLAM = PHOTFNU * (const.c.cgs/Pivot_wave.cgs).to(u.Hz) / (Pivot_wave).to(u.AA)
+    logger.info('PHOTFLAM: {}'.format(PHOTFLAM))
     STMAG = ABMAG.to(u.STmag, u.spectral_density(Pivot_wave))
+    logger.info('STMAG: {}'.format(STMAG))
     #VEGAMAG = -2.5*np.log10(PHOTFNU.value) - 48.6
     VEGAMAG = PHOTFNU.to(u.ABmag).value
     flux_cal_dict['VEGAMAG'] = VEGAMAG
