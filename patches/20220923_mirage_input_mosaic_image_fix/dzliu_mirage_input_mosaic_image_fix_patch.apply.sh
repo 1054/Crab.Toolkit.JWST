8f9a8181673e23584de3e7ec479f3d8bb06b84a2
1054 <>
2022-11-18 18:56:59 +0100

    Updates

---
+++ patches/20220923_mirage_input_mosaic_image_fix/dzliu_mirage_input_mosaic_image_fix_patch.apply.sh
@@ -0,0 +1,15 @@
+patch \
+Software/CONDA/miniconda3/envs/miragepmap1009/lib/python3.9/site-packages/mirage/seed_image/fits_seed_image.py \
+< dzliu_mirage_input_mosaic_image_fix_patch1.txt
+
+patch \
+Software/CONDA/miniconda3/envs/miragepmap1009/lib/python3.9/site-packages/mirage/seed_image/fits_seed_image.py \
+< dzliu_mirage_input_mosaic_image_fix_patch2.txt
+
+patch \
+Software/CONDA/miniconda3/envs/miragepmap1009/lib/python3.9/site-packages/mirage/seed_image/fits_seed_image.py \
+< dzliu_mirage_input_mosaic_image_fix_patch3.txt
+
+patch \
+Software/CONDA/miniconda3/envs/miragepmap1009/lib/python3.9/site-packages/mirage/seed_image/fits_seed_image.py \
+< dzliu_mirage_input_mosaic_image_fix_patch4.txt
---
+++ patches/20220923_mirage_input_mosaic_image_fix/dzliu_mirage_input_mosaic_image_fix_patch.create.sh
@@ -0,0 +1,19 @@
+diff -u \
+Software/CONDA/miniconda3/envs/jwstpmap1009/lib/python3.9/site-packages/mirage/seed_image/fits_seed_image.py \
+Cloud/Github/mirage/seed_image/fits_seed_image.py \
+> dzliu_mirage_input_mosaic_image_fix_patch1.txt
+
+diff -u \
+Software/CONDA/miniconda3/envs/jwstpmap1009/lib/python3.9/site-packages/mirage/seed_image/blot_image.py \
+Cloud/Github/mirage/seed_image/blot_image.py \
+> dzliu_mirage_input_mosaic_image_fix_patch2.txt
+
+diff -u \
+Software/CONDA/miniconda3/envs/jwstpmap1009/lib/python3.9/site-packages/mirage/seed_image/save_seed.py \
+Cloud/Github/mirage/seed_image/save_seed.py \
+> dzliu_mirage_input_mosaic_image_fix_patch3.txt
+
+diff -u \
+Software/CONDA/miniconda3/envs/jwstpmap1009/lib/python3.9/site-packages/jwst/outlier_detection/outlier_detection.py \
+Software/CONDA/miniconda3/envs/miragepmap1009/lib/python3.9/site-packages/jwst/outlier_detection/outlier_detection.py \
+> dzliu_mirage_input_mosaic_image_fix_patch4.txt
