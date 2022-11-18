dd8a06e360da5248fe42a0706c01d613928e7756
1054 <>
2022-11-18 18:57:37 +0100

    Updates

--- patches/20220923_mirage_input_mosaic_image_fix/dzliu_mirage_input_mosaic_image_fix_patch.apply.sh
+++ patches/20220923_mirage_input_mosaic_image_fix/dzliu_mirage_input_mosaic_image_fix_patch.apply.sh
@@ -3,13 +3,13 @@ Software/CONDA/miniconda3/envs/miragepmap1009/lib/python3.9/site-packages/mirage
 < dzliu_mirage_input_mosaic_image_fix_patch1.txt
 
 patch \
-Software/CONDA/miniconda3/envs/miragepmap1009/lib/python3.9/site-packages/mirage/seed_image/fits_seed_image.py \
+Software/CONDA/miniconda3/envs/miragepmap1009/lib/python3.9/site-packages/mirage/seed_image/blot_image.py \
 < dzliu_mirage_input_mosaic_image_fix_patch2.txt
 
 patch \
-Software/CONDA/miniconda3/envs/miragepmap1009/lib/python3.9/site-packages/mirage/seed_image/fits_seed_image.py \
+Software/CONDA/miniconda3/envs/miragepmap1009/lib/python3.9/site-packages/mirage/seed_image/save_seed.py \
 < dzliu_mirage_input_mosaic_image_fix_patch3.txt
 
 patch \
-Software/CONDA/miniconda3/envs/miragepmap1009/lib/python3.9/site-packages/mirage/seed_image/fits_seed_image.py \
+Software/CONDA/miniconda3/envs/miragepmap1009/lib/python3.9/site-packages/jwst/outlier_detection/outlier_detection.py \
 < dzliu_mirage_input_mosaic_image_fix_patch4.txt
