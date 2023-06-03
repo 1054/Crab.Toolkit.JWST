fab82bbcfd7fe0d6a41880542e4d3a8779b74cae
1054 <>
2023-06-03 21:09:00 +0200

    Updates

--- bin/util_get_image_wcs_as_json_file.py
+++ bin/util_get_image_wcs_as_json_file.py
@@ -72,7 +72,7 @@ def get_one_image_wcs(
     wcs_dict['FILENAME'] = image_file
     for key in wcs_header:
         wcs_dict[key] = wcs_header[key]
-    wcs_dict['S_REGION'] = 'poylgon ' + ' '.join(['{:.8f}'.format(t) for t in footprint])
+    wcs_dict['S_REGION'] = 'polygon ' + ' '.join(['{:.8f}'.format(t) for t in footprint])
     return wcs_dict
 
 
