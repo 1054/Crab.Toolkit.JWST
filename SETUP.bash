9f1dcbad353e066a8c99d10deafb1d18490d9a46
1054 <>
2023-10-11 11:27:22 +0200

    Updates

--- SETUP.bash
+++ SETUP.bash
@@ -5,7 +5,7 @@
 Crab_BIN_SETUP_SCRIPT=$(dirname "${BASH_SOURCE[0]}")/bin/bin_setup.bash
 
 if [[ $- == *i* ]]; then
-    source "$Crab_BIN_SETUP_SCRIPT" -check go-jwst-download-by-proposal-id.py
+    source "$Crab_BIN_SETUP_SCRIPT" -check go-jwst-query-by-program-id.py
 else
     source "$Crab_BIN_SETUP_SCRIPT"
 fi
--- SETUP.fish
+++ SETUP.fish
@@ -8,9 +8,9 @@ set BIN_SETUP_SCRIPT (dirname (status --current-filename))/bin/bin_setup.bash
 #echo "PATH = $PATH"
 #echo 
 
-set -x PATH (string split ":" (bash -c "source $BIN_SETUP_SCRIPT -debug -check go-jwst-download-by-proposal-id.py -print" | tail -n 1))
+set -x PATH (string split ":" (bash -c "source $BIN_SETUP_SCRIPT -debug -check go-jwst-query-by-program-id.py -print" | tail -n 1))
 
-type go-jwst-download-by-proposal-id.py
+type go-jwst-query-by-program-id.py
 
 #echo 
 #echo "PATH = $PATH"
