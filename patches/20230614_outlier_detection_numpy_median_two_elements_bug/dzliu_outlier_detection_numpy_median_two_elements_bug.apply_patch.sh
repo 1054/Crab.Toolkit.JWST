62ef10744090f3df7f66c03d8c97b65e3c6c2d38
1054 <>
2024-01-11 17:30:55 +0100

    Updates

--- patches/20230614_outlier_detection_numpy_median_two_elements_bug/dzliu_outlier_detection_numpy_median_two_elements_bug.apply_patch.sh
+++ patches/20230614_outlier_detection_numpy_median_two_elements_bug/dzliu_outlier_detection_numpy_median_two_elements_bug.apply_patch.sh
@@ -7,12 +7,15 @@ if [[ -z $CONDA_PREFIX ]]; then
     exit 255
 fi
 
-if [[ ! -f $CONDA_PREFIX/lib/python3.9/site-packages/jwst/outlier_detection/outlier_detection.py.ori ]]; then
-    cp -i $CONDA_PREFIX/lib/python3.9/site-packages/jwst/outlier_detection/outlier_detection.py \
-        $CONDA_PREFIX/lib/python3.9/site-packages/jwst/outlier_detection/outlier_detection.py.ori
+script_dir=$(dirname ${BASH_SOURCE[0]})
+site_packages_dir=$(python -c 'from distutils.sysconfig import get_python_lib; print(get_python_lib())')
+
+if [[ ! -f ${site_packages_dir}/jwst/outlier_detection/outlier_detection.py.ori ]]; then
+    cp -i ${site_packages_dir}/jwst/outlier_detection/outlier_detection.py \
+          ${site_packages_dir}/jwst/outlier_detection/outlier_detection.py.ori
 fi
 
 patch \
-$CONDA_PREFIX/lib/python3.9/site-packages/jwst/outlier_detection/outlier_detection.py \
-< $(dirname ${BASH_SOURCE[0]})/dzliu_outlier_detection_numpy_median_two_elements_bug.diff
+    ${site_packages_dir}/jwst/outlier_detection/outlier_detection.py \
+    < ${script_dir}/dzliu_outlier_detection_numpy_median_two_elements_bug.diff
 
