100f0993edfa20dca4dd08595845c7b902a11f19
1054 <>
2024-02-13 15:59:11 +0100

    Updates

--- patches/20230109_tweakreg_step_model_container_from_asn_bug/dzliu_tweakreg_step_model_container_from_asn_bug.apply_patch.sh
+++ patches/20230109_tweakreg_step_model_container_from_asn_bug/dzliu_tweakreg_step_model_container_from_asn_bug.apply_patch.sh
@@ -7,12 +7,25 @@ if [[ -z $CONDA_PREFIX ]]; then
     exit 255
 fi
 
-if [[ ! -f $CONDA_PREFIX/lib/python3.9/site-packages/jwst/tweakreg/tweakreg_step.py.backup ]]; then
-    cp -i $CONDA_PREFIX/lib/python3.9/site-packages/jwst/tweakreg/tweakreg_step.py \
-        $CONDA_PREFIX/lib/python3.9/site-packages/jwst/tweakreg/tweakreg_step.py.backup
+site_packages_dir=$(python -c 'import site; print(site.getsitepackages()[0])')
+if [[ x"$site_packages_dir" == x"" ]]; then
+    echo "Error! Could not get Python site-packages directory! Please check: "
+    echo "    python -c 'import site; print(site.getsitepackages()[0])'"
+    exit
+fi
+
+if [[ ! -f $site_packages_dir/jwst/tweakreg/tweakreg_step.py ]]; then
+    echo "Error! File not found: "
+    echo "    $site_packages_dir/jwst/tweakreg/tweakreg_step.py"
+    exit
+fi
+
+if [[ ! -f $site_packages_dir/jwst/tweakreg/tweakreg_step.py.backup ]]; then
+    cp -i $site_packages_dir/jwst/tweakreg/tweakreg_step.py \
+        $site_packages_dir/jwst/tweakreg/tweakreg_step.py.backup
 fi
 
 patch \
-$CONDA_PREFIX/lib/python3.9/site-packages/jwst/tweakreg/tweakreg_step.py \
+$site_packages_dir/jwst/tweakreg/tweakreg_step.py \
 < $(dirname ${BASH_SOURCE[0]})/dzliu_tweakreg_step_model_container_from_asn_bug.diff
 
