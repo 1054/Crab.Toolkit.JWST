6bb6ca9497287ea690329866e002985b00516397
1054 <1054@users.noreply.github.com>
2024-06-19 00:34:01 +0800

    Mirage bug obs generator lam too large

---
+++ patches/20230522_mirage_obs_generator_signalgain_nan_lam_too_large/dzliu_mirage_obs_generator_signalgain_nan_lam_too_large_fix_patch.apply.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# 
+set -e
+
+if [[ -z $CONDA_PREFIX ]]; then
+    echo "Please set your conda environment \$CONDA_PREFIX first!"
+    exit 255
+fi
+
+script_dir=$(dirname ${BASH_SOURCE[0]})
+site_packages_dir=$(python -c 'from distutils.sysconfig import get_python_lib; print(get_python_lib())')
+
+if [[ ! -f ${site_packages_dir}/mirage/ramp_generator/obs_generator.py.ori ]]; then
+    cp -i ${site_packages_dir}/mirage/ramp_generator/obs_generator.py \
+          ${site_packages_dir}/mirage/ramp_generator/obs_generator.py.ori
+fi
+
+patch \
+    ${site_packages_dir}/mirage/ramp_generator/obs_generator.py \
+    < ${script_dir}/dzliu_seed_image/fits_seed_image_fix_patch.txt
+
+
+# 20240619: still needed for jwst==1.14.0 and mirage==2.4.0
---
+++ patches/20230522_mirage_obs_generator_signalgain_nan_lam_too_large/dzliu_mirage_obs_generator_signalgain_nan_lam_too_large_fix_patch.create.sh
@@ -0,0 +1,5 @@
+diff -u \
+~/Software/CONDA/miniconda3/envs/jwstpmap1230/lib/python3.10/site-packages/mirage/ramp_generator/obs_generator.py \
+~/Software/CONDA/miniconda3/envs/jwstpmap1130/lib/python3.10/site-packages/mirage/ramp_generator/obs_generator.py \
+> dzliu_mirage_obs_generator_signalgain_nan_lam_too_large_fix_patch.txt  
+
